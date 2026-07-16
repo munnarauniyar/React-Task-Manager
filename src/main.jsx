4ce3e4e201604e1020f2aa16a359349325437977
Munna Rauniyar <munarauniyar100@gmail.com>
2026-07-16 11:43:08 +0545

    final changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDom.createRoot(document.getElementById('root')).render(
